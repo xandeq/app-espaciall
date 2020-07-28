51e57b4182f6a4c37e154cd2ecc694d350061e16
xandeq <xandeq@gmail.com>
2020-07-28 18:28:10 -0300

    segundo

--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -8,21 +8,36 @@ import { map, catchError } from "rxjs/operators";
   providedIn: "root",
 })
 export class NasaService {
-  baseurl =
-    "https://api.nasa.gov/planetary/apod?api_key=BzCHxf3K4h1enx5cUDETYFuGaexAd9NaOib69ZKy&start_date=2020-02-10&end_date=2020-02-15";
+  API_KEY = "BzCHxf3K4h1enx5cUDETYFuGaexAd9NaOib69ZKy";
+  baseurl = "https://api.nasa.gov/planetary/apod?api_key=" + this.API_KEY;
 
   constructor(private httpClient: HttpClient) {}
 
-  // getApod() {
-  //   return this.httpClient.get(this.baseurl);
-  // }
-
   public getApods(): Observable<Apod[]> {
-    return this.httpClient.get<Apod[]>(this.baseurl).pipe(
+    const hoje: Date = new Date();
+    const inicio =
+      hoje.getFullYear() + "-" + (hoje.getMonth() + 1) + "-" + hoje.getDate();
+    console.log(this.baseurl + "&date=" + inicio);
+    return this.httpClient.get<Apod[]>(this.baseurl + "&date=" + inicio).pipe(
       map((res) => res.map((apod) => new Apod(apod))),
       catchError((err) => {
         throw "error in source. Details: " + err;
       })
     );
+  }
+
+  randomDate(date1, date2) {
+    function randomValueBetween(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+    var date1 = date1 || "01-01-1970";
+    var date2 = date2 || new Date().toLocaleDateString();
+    date1 = new Date(date1).getTime();
+    date2 = new Date(date2).getTime();
+    if (date1 > date2) {
+      return new Date(randomValueBetween(date2, date1)).toLocaleDateString();
+    } else {
+      return new Date(randomValueBetween(date1, date2)).toLocaleDateString();
+    }
   }
 }
