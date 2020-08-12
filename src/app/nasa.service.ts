5b4c198d45b4357f56319b9f62657b4d0a1faaeb
xandeq <xandeq@gmail.com>
2020-08-12 20:49:13 -0300

    Data aleatoria

--- src/app/nasa.service.ts
+++ src/app/nasa.service.ts
@@ -14,30 +14,43 @@ export class NasaService {
   constructor(private httpClient: HttpClient) {}
 
   public getApods(): Observable<Apod[]> {
-    const hoje: Date = new Date();
-    const inicio =
-      hoje.getFullYear() + "-" + (hoje.getMonth() + 1) + "-" + hoje.getDate();
-    console.log(this.baseurl + "&date=" + inicio);
-    return this.httpClient.get<Apod[]>(this.baseurl + "&date=" + inicio).pipe(
-      map((res) => res.map((apod) => new Apod(apod))),
-      catchError((err) => {
-        throw "error in source. Details: " + err;
-      })
-    );
+    return this.httpClient
+      .get<Apod[]>(
+        this.baseurl +
+          "&start_date=" +
+          this.getEndDate() +
+          "&end_date=" +
+          this.getStartDate()
+      )
+      .pipe(
+        map((res) => res.map((apod) => new Apod(apod))),
+        catchError((err) => {
+          throw "error in source. Details: " + err;
+        })
+      );
   }
 
-  randomDate(date1, date2) {
-    function randomValueBetween(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-    var date1 = date1 || "01-01-1970";
-    var date2 = date2 || new Date().toLocaleDateString();
-    date1 = new Date(date1).getTime();
-    date2 = new Date(date2).getTime();
-    if (date1 > date2) {
-      return new Date(randomValueBetween(date2, date1)).toLocaleDateString();
-    } else {
-      return new Date(randomValueBetween(date1, date2)).toLocaleDateString();
-    }
+  getStartDate(): string {
+    const today = new Date();
+    return this.formatDate(today);
+  }
+
+  getEndDate(): string {
+    const today = new Date();
+    const tenDaysBefore = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() - 10
+    );
+    return this.formatDate(tenDaysBefore);
+  }
+
+  formatDate(date): string {
+    let dd = date.getDate();
+    let MM = date.getMonth();
+    const yyyy = date.getFullYear();
+    dd = dd < 10 ? `0${dd}` : dd;
+    MM = MM < 10 ? `0${MM}` : MM;
+    return `${yyyy}-${MM}-${dd}`;
   }
 }
